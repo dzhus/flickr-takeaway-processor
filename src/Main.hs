4606ffbfbcb2ec2919f7d74462ee907e3327bc73
Dmitry Dzhus <dima@dzhus.org>
2018-11-04 16:26:17 +0000

    feat: Parse sidecar jsons

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,84 @@
 module Main where
 
+import ClassyPrelude hiding (FilePath)
+import Data.Aeson as A hiding (Options)
+import qualified Data.Text as T
+import Data.Text.Read
+import qualified Control.Foldl as Fold
+import Turtle hiding (o)
+
+data Options = Options
+  { metaDir  :: FilePath
+  , mediaDir :: FilePath
+  }
+
+newtype SpacedUTCTime = SpacedUTCTime { unwrap :: UTCTime }
+  deriving (Show)
+
+instance FromJSON SpacedUTCTime where
+  parseJSON =
+    withText "SpacedUTCTime" $
+    fmap SpacedUTCTime . parseJSON . String .
+    -- Flickr sidecars contain no T separator and no timezone
+    T.replace " " "T" . (<> "Z")
+
+data Geo = Geo
+  { latitude  :: Float
+  , longitude :: Float
+  }
+  deriving Show
+
+instance FromJSON Geo where
+  parseJSON =
+    withObject "Geo" $
+    \o -> Geo <$> (conv =<< (o .: "latitude")) <*> (conv =<< (o .: "longitude"))
+    where
+      conv txt = case rational txt of
+                   Right (num, _) -> return $ num / 1e6
+                   _              -> fail "Could not read Geo components"
+
+newtype Tag = Tag { unwrap :: Text }
+  deriving Show
+
+instance FromJSON Tag where
+  parseJSON = withObject "Tag" $ \o -> Tag <$> (o .: "tag")
+
+newtype Album = Album { unwrap :: Text }
+  deriving Show
+
+instance FromJSON Album where
+  parseJSON = withObject "Album" $ \o -> Album <$> (o .: "title")
+
+data PhotoMeta = PhotoMeta
+  { id          :: Text
+  , date_taken  :: SpacedUTCTime
+  , description :: Text
+  , name        :: Text
+  , geo         :: Maybe Geo
+  , albums      :: [Album]
+  , tags        :: [Tag]
+  }
+  deriving (Generic, Show)
+
+instance FromJSON PhotoMeta
+
+optParser :: Parser Options
+optParser = Options
+  <$> argPath "meta" "Directory containing photo_*.json files"
+  <*> argPath "media" "Directory with media files"
+
+parseSidecar :: MonadIO m => FilePath -> m (Maybe PhotoMeta)
+parseSidecar jf = do
+  res <- liftIO $ ClassyPrelude.readFile $ encodeString jf
+  return $ decode $ fromStrict res
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  Options{..} <- options "Process Flickr takeaway files" optParser
+  files <- flip Turtle.fold Fold.list $ do
+    pushd metaDir
+    Turtle.find (contains "photo_" *> suffix ".json") =<< pwd
+  print $ headMay files
+  jsons <- mapConcurrently parseSidecar files
+  print $ length $ catMaybes jsons
+  print $ headMay jsons
